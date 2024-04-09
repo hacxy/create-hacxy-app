1461c4313d7241712363df78465814429dc63175
Hacxy <hacxy.97@outlook.com>
2024-04-09 18:38:39 +0800

    init: 项目初始化

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+import prompts from "prompts";
+import path from "node:path";
+import fs from "node:fs";
+const bootstrap = async () => {
+  const result = await prompts([
+    {
+      type: "text",
+      name: "projectName",
+      message: "请输入项目名称: ",
+    },
+  ]);
+
+  const targetPath = path.resolve(process.cwd(), result.projectName);
+  const sourcePath = path.resolve(__dirname, "../template");
+
+  fs.cpSync(sourcePath, targetPath, {
+    recursive: true,
+  });
+
+  fs.renameSync(
+    path.resolve(targetPath, "_gitignore"),
+    path.resolve(targetPath, ".gitignore")
+  );
+
+  console.log(`
+  创建成功 !! 🥳
+
+  cd ${result.projectName}
+  npm run install
+  npm run dev
+  `);
+};
+
+bootstrap();
